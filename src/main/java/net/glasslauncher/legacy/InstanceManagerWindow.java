ad4357e42993b9c3a221c61a0c0455a19ab8ede9
calmilamsy <calmilamsy@pymcl.net>
2019-11-04 01:57:43 +0000

    Added blank instance creation and modpack zip downloading
    Next step: actual modpack installation.

--- src/main/java/net/glasslauncher/legacy/InstanceManagerWindow.java
+++ src/main/java/net/glasslauncher/legacy/InstanceManagerWindow.java
@@ -1,5 +1,9 @@
 package net.glasslauncher.legacy;
 
+import net.glasslauncher.legacy.components.HintTextField;
+import net.glasslauncher.legacy.util.InstanceManager;
+import net.glasslauncher.legacy.util.JsonConfig;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.WindowAdapter;
@@ -22,8 +26,65 @@ class InstanceManagerWindow extends JDialog {
         setBounds(0, 0, 580, 340);
 
         JTabbedPane tabbedPane = new JTabbedPane();
+        tabbedPane.setBounds(0, 0, 580, 340);
+        tabbedPane.addTab("Create", makeCreateTab());
+
+        add(tabbedPane);
 
         setLocationRelativeTo(null);
         setVisible(true);
+    }
+
+    private Panel makeCreateTab() {
+        Panel createPane = new Panel();
+        createPane.setBounds(0, 0, 580, 340);
+        createPane.setLayout(null);
+
+        // Local Modpack zip install.
+        HintTextField installModpackDir = new HintTextField("Paste the path to your Modpack zip here!");
+        installModpackDir.setBounds(155, 5, 390, 22);
+
+        JButton installModpackDirButton = new JButton();
+        installModpackDirButton.setText("Install Local Modpack");
+        installModpackDirButton.setBounds(5, 5, 150, 22);
+        installModpackDirButton.addActionListener(event -> {
+            InstanceManager.installModpack(installModpackDir.getText());
+        });
+
+        // Remote Modpack zip install.
+        HintTextField installModpackURL = new HintTextField("Paste the URL to your Modpack zip here!");
+        installModpackURL.setBounds(155, 34, 390, 22);
+        installModpackDirButton.addActionListener(event -> {
+            InstanceManager.installModpack(installModpackURL.getText());
+        });
+
+        JButton installModpackURLButton = new JButton();
+        installModpackURLButton.setText("Install Modpack from URL");
+        installModpackURLButton.setBounds(5, 34, 150, 22);
+
+        HintTextField instanceName = new HintTextField("Instance name");
+        instanceName.setBounds(155, 63, 194, 22);
+
+        JComboBox instanceVersion = new JComboBox();
+        for (Object version : (new JsonConfig(Main.class.getResource("assets/mcversions.json").toString())).keySet()) {
+            instanceVersion.addItem(version);
+        }
+        instanceVersion.setBounds(351, 63, 194, 22);
+
+        JButton instanceVersionButton = new JButton();
+        instanceVersionButton.setText("Create Blank Instance");
+        instanceVersionButton.addActionListener((e) -> {
+            InstanceManager.createBlankInstance((String) instanceVersion.getSelectedItem(), instanceName.getText());
+        });
+        instanceVersionButton.setBounds(5, 63, 150, 22);
+
+        createPane.add(installModpackDir);
+        createPane.add(installModpackDirButton);
+        createPane.add(installModpackURL);
+        createPane.add(installModpackURLButton);
+        createPane.add(instanceName);
+        createPane.add(instanceVersion);
+        createPane.add(instanceVersionButton);
+        return createPane;
     }
 }
---
+++ src/main/java/net/glasslauncher/legacy/util/InstanceManager.java
@@ -0,0 +1,95 @@
+package net.glasslauncher.legacy.util;
+
+import com.cedarsoftware.util.io.JsonObject;
+import net.glasslauncher.legacy.Config;
+import net.glasslauncher.legacy.Main;
+
+import java.io.File;
+import java.net.URL;
+import java.nio.file.Files;
+
+public class InstanceManager {
+
+    /**
+     * Detects what kind of modpack zip has been provided and then calls the related install function for the type.
+     * @param path Path to instance zip file.
+     * @return true if succeeded, false otherwise.
+     */
+    public static boolean installModpack(String path) {
+        path = path.replace("\\", "/");
+        Main.logger.info("Installing " + path);
+        boolean isURL = true;
+        try {
+            try {
+                new URL(path);
+            } catch (Exception e) {
+                isURL = false;
+            }
+
+            String filename = path.substring(path.lastIndexOf('/') + 1);
+            if (isURL) {
+                FileUtils.downloadFile(path, Config.cachepath + "instancezips");
+                installModpackZip(Config.cachepath + "instancezips", filename);
+            } else {
+                if ((new File(path)).exists()) {
+                    installModpackZip(path, filename);
+                } else {
+                    return false;
+                }
+            }
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    private static boolean installModpackZip(String path, String filename) {
+        Main.logger.info(path + " : " + filename);
+        createBlankInstance("b1.7.3", filename);
+        return true;
+    }
+
+    public static boolean createBlankInstance(String version, String name) {
+        Main.logger.info("Creating instance \"" + name + "\" on version " + version);
+        String versionsCachePath = Config.cachepath + "versions";
+        String instanceFolder = Config.glasspath + "instances/" + name;
+        String minecraftFolder = instanceFolder + "/.minecraft";
+        (new File(versionsCachePath)).mkdirs();
+        (new File(minecraftFolder + "/bin/")).mkdirs();
+
+        File versionCacheJar = new File(versionsCachePath + "/" + version + ".jar");
+        if (versionCacheJar.exists()) {
+            try {
+                Files.copy(versionCacheJar.toPath(), new File(minecraftFolder + "/bin/minecraft.jar").toPath());
+            } catch (Exception e) {
+                e.printStackTrace();
+                try {
+                    org.apache.commons.io.FileUtils.deleteDirectory(new File(minecraftFolder));
+                } catch (Exception ignored) {}
+                return false;
+            }
+        } else {
+            JsonConfig jsonVersions = new JsonConfig(Main.class.getResource("assets/mcversions.json").toString());
+
+            try {
+                String versionID = (String) ((JsonObject) jsonVersions.get(version)).get("url");
+                FileUtils.downloadFile("https://launcher.mojang.com/v1/objects/" + versionID + "/client.jar", versionsCachePath, null, version + ".jar");
+                Files.copy(versionCacheJar.toPath(), new File(minecraftFolder + "/bin/minecraft.jar").toPath());
+            } catch (Exception e) {
+                try {
+                    org.apache.commons.io.FileUtils.deleteDirectory(new File(minecraftFolder));
+                } catch (Exception ignored) {}
+                Main.logger.info("Version not found: \"" + version + "\". Aborting.");
+                return false;
+            }
+        }
+        File lwjglCacheZip = new File(versionsCachePath + "/lwjgl.zip");
+        if (!lwjglCacheZip.exists()) {
+            FileUtils.downloadFile("https://files.pymcl.net/client/lwjgl/lwjgl." + Config.os + ".zip", versionsCachePath, null, "lwjgl.zip");
+        }
+        FileUtils.extractZip(lwjglCacheZip.getPath(), minecraftFolder + "/bin");
+
+        return true;
+    }
+}
