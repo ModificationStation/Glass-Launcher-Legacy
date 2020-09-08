0a8d55340061488fbdc0823ce2bb13cff23ec659
calmilamsy <calmilamsy@pymcl.net>
2020-09-08 20:36:35 +0100

    Change background for ModRepoList

--- src/main/java/net/glasslauncher/legacy/components/ModRepoList.java
+++ src/main/java/net/glasslauncher/legacy/components/ModRepoList.java
@@ -6,6 +6,7 @@ import net.glasslauncher.repo.api.mod.jsonobj.ModPreview;
 import javax.swing.DefaultListCellRenderer;
 import javax.swing.DefaultListModel;
 import javax.swing.JList;
+import java.awt.Color;
 import java.awt.Component;
 import java.util.List;
 
@@ -20,7 +21,9 @@ public class ModRepoList extends JList<ModPreview> {
             public Component getListCellRendererComponent(JList list, Object value, int index, boolean isSelected, boolean cellHasFocus) {
                 Component c = super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
                 ModPreview mod = (ModPreview) value;
-                setText(mod.getName());
+                setText(" " + mod.getName());
+                setBackground(new Color(52, 52, 52));
+                setForeground(new Color(218, 218, 218));
                 if (isSelected && !lastRendered.equals(mod.getId())) {
                     setBackground(getBackground().darker());
                     try {
