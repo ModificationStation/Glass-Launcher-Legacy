9cb53121c8c1be92a48df6cc44bf9092a44fdd3e
kyle cheeseman <bumbill00@gmail.com>
2021-04-24 12:20:58 +0100

    Made the confirmation prompt a bit prettier

--- src/main/java/net/glasslauncher/legacy/OptionsWindow.java
+++ src/main/java/net/glasslauncher/legacy/OptionsWindow.java
@@ -108,7 +108,7 @@ public class OptionsWindow extends JDialog {
                         }
                     }
                     if (!modCompatChecked) {
-                        int response = JOptionPane.showConfirmDialog(parent, "Jar mod compatibility has not been checked!\nAre you sure you don't want to run a check to see if any mods conflict?");
+                        int response = JOptionPane.showOptionDialog(parent, "Jar mod compatibility has not been checked!\nAre you sure you don't want to run a check to see if any mods conflict?", "Confirm", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null, null, null);
                         if (response != JOptionPane.YES_OPTION) {
                             checkModCompat();
                             return;
