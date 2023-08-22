4aac2a0eb9675562c7ef9ce82d8d5f92e1f93d8c
calmilamsy <bumbill00@gmail.com>
2023-08-22 16:55:16 +0100

    Make JFX no longer required for compile

--- src/main/java/net/glasslauncher/legacy/util/LinkRedirector.java
+++ src/main/java/net/glasslauncher/legacy/util/LinkRedirector.java
@@ -1,53 +1,11 @@
 package net.glasslauncher.legacy.util;
 
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
-import javafx.concurrent.Worker;
-import javafx.scene.web.WebView;
 import net.glasslauncher.legacy.Main;
-import org.w3c.dom.Document;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
-import org.w3c.dom.events.Event;
-import org.w3c.dom.events.EventListener;
-import org.w3c.dom.events.EventTarget;
-import org.w3c.dom.html.HTMLAnchorElement;
 
 import java.awt.*;
 import java.net.*;
 
-public class LinkRedirector implements ChangeListener<Worker.State>, EventListener {
-    private static final String CLICK_EVENT = "click";
-    private static final String ANCHOR_TAG = "a";
-
-    private final WebView webView;
-
-    public LinkRedirector(WebView webView) {
-        this.webView = webView;
-    }
-
-    @Override
-    public void changed(ObservableValue<? extends Worker.State> observable, Worker.State oldValue, Worker.State newValue) {
-        if (Worker.State.SUCCEEDED.equals(newValue)) {
-            Document document = webView.getEngine().getDocument();
-            NodeList anchors = document.getElementsByTagName(ANCHOR_TAG);
-            for (int i = 0; i < anchors.getLength(); i++) {
-                Node node = anchors.item(i);
-                EventTarget eventTarget = (EventTarget) node;
-                eventTarget.addEventListener(CLICK_EVENT, this, false);
-            }
-        }
-    }
-
-    @Override
-    public void handleEvent(Event event) {
-        HTMLAnchorElement anchorElement = (HTMLAnchorElement) event.getCurrentTarget();
-        String href = anchorElement.getHref();
-
-        openLinkInSystemBrowser(href);
-
-        event.preventDefault();
-    }
+public class LinkRedirector {
 
     public static void openLinkInSystemBrowser(String url) {
         if (Desktop.isDesktopSupported()) {
